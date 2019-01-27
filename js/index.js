22e7890e3a19117c66343826f3e15115d0f7ed4a
Jeffery <e_love12134@hotmail.com>
2019-01-28 00:11:34 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@
                 company: '',
                 email: '',
                 phone: '',
-                msg: '',
+                msg: '', 
             }
         },
         computed: {},
