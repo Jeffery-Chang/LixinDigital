86685ede4577dd0355441e92f44a3e598de8184b
Jeffery <e_love12134@hotmail.com>
2019-02-02 00:54:01 +0800

    Update index.bundle.js

--- js/index.bundle.js
+++ js/index.bundle.js
@@ -18,13 +18,6 @@ jQuery.getUrlParam = function(b) {
             msg: ""
         }
     },
-    computed: {
-        clipStyle: function() {
-            if (11 < this.chkIE()) return {}; else return {
-                transform: "scale(1.35)"
-            };
-        }
-    },
     created: function() {
         window.addEventListener("scroll", this.ctrlScroll), window.addEventListener("orientationchange", this.avoidAnriod);
     },
@@ -114,50 +107,54 @@ jQuery.getUrlParam = function(b) {
             });
         },
         mediaEffect: function() {
-            function Circle(x, y, dx, dy, radius, isImg, img_src) {
-                this.x = x, this.y = y, this.dx = dx, this.dy = dy, this.radius = radius, this.minRadius = radius, 
-                this.color = colorArray[Math.floor(Math.random() * colorArray.length)], this.isImg = isImg || !1, 
-                this.img_src = img_src || null, this.draw = function() {
-                    if (this.isImg) c.drawImage(this.img_src, this.x, this.y, this.radius, this.radius); else c.beginPath(), 
-                    c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, !1), c.fillStyle = this.color, 
-                    c.fill();
-                }, this.update = function() {
-                    if (this.x + this.radius > canvas.width || this.x - this.radius < 0) this.dx = -this.dx;
-                    if (this.y + this.radius > canvas.height || this.y - this.radius < 0) this.dy = -this.dy;
-                    this.y += this.dy, this.x += this.dx;
-                    var max = this.isImg ? imgMaxRadius : maxRadius;
-                    if (mouse.x - this.x - this.radius / 2 < 100 && -100 < mouse.x - this.x - this.radius / 2 && mouse.y - this.y - this.radius / 2 < 100 && -100 < mouse.y - this.y - this.radius / 2) {
-                        if (this.radius < max) this.radius += 1;
-                    } else if (this.radius > this.minRadius) this.radius -= 1;
-                    this.draw();
-                };
-            }
-            var canvas = document.querySelector("canvas");
-            canvas.width = window.innerWidth, canvas.height = isMobile.phone ? window.innerHeight : 625, 
-            canvas.style.width = canvas.width + "px", canvas.style.height = canvas.height + "px";
-            var c = canvas.getContext("2d"), mouse = {
-                x: void 0,
-                y: void 0
-            }, maxRadius = 25, imgMaxRadius = 150, colorArray = [ "rgba(99, 84, 75, 0.61)", "rgba(196, 126, 33, 0.6)", "rgba(194, 138, 48, 0.5)", "rgba(79, 69, 63, 0.65)" ];
-            window.addEventListener("mousemove", function(event) {
-                mouse.x = event.pageX, mouse.y = event.pageY - document.querySelector(".mediaPartner").offsetTop;
-            }), window.addEventListener("resize", function() {
-                canvas.width = window.innerWidth, canvas.height = isMobile.phone ? window.innerHeight : 625, 
-                canvas.style.width = canvas.width + "px", canvas.style.height = canvas.height + "px";
+            var canvas = document.querySelector("canvas"), c = canvas.getContext("2d"), quality = c.backingStorePixelRatio || c.webkitBackingStorePixelRatio || c.mozBackingStorePixelRatio || c.msBackingStorePixelRatio || c.oBackingStorePixelRatio || c.backingStorePixelRatio || 1, ratio = (window.devicePixelRatio || 1) / quality;
+            console.log(ratio), canvas.width = window.innerWidth * ratio, canvas.height = (isMobile.phone ? window.innerHeight : 625) * ratio, 
+            canvas.style.width = window.innerWidth + "px", canvas.style.height = isMobile.phone ? window.innerHeight : "625px", 
+            this.$nextTick(function() {
+                function Circle(x, y, dx, dy, radius, isImg, img_src) {
+                    this.x = x, this.y = y, this.dx = dx, this.dy = dy, this.radius = radius, this.minRadius = radius, 
+                    this.color = colorArray[Math.floor(Math.random() * colorArray.length)], this.isImg = isImg || !1, 
+                    this.img_src = img_src || null, this.draw = function() {
+                        if (this.isImg) c.drawImage(this.img_src, this.x, this.y, this.radius, this.radius); else c.beginPath(), 
+                        c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, !1), c.fillStyle = this.color, 
+                        c.fill();
+                    }, this.update = function() {
+                        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) this.dx = -this.dx;
+                        if (this.y + this.radius > canvas.height || this.y - this.radius < 0) this.dy = -this.dy;
+                        this.y += this.dy, this.x += this.dx;
+                        var max = this.isImg ? imgMaxRadius : maxRadius;
+                        if (mouse.x - this.x - this.radius / 2 < 100 && -100 < mouse.x - this.x - this.radius / 2 && mouse.y - this.y - this.radius / 2 < 100 && -100 < mouse.y - this.y - this.radius / 2) {
+                            if (this.radius < max) this.radius += 1;
+                        } else if (this.radius > this.minRadius) this.radius -= 1;
+                        this.draw();
+                    };
+                }
+                c.setTransform(ratio, 0, 0, ratio, 0, 0);
+                var mouse = {
+                    x: void 0,
+                    y: void 0
+                }, maxRadius = 25, imgMaxRadius = 150, colorArray = [ "rgba(99, 84, 75, 0.61)", "rgba(196, 126, 33, 0.6)", "rgba(194, 138, 48, 0.5)", "rgba(79, 69, 63, 0.65)" ];
+                window.addEventListener("mousemove", function(event) {
+                    mouse.x = event.pageX, mouse.y = event.pageY - document.querySelector(".mediaPartner").offsetTop;
+                }), window.addEventListener("resize", function() {
+                    canvas.width = window.innerWidth * ratio, canvas.height = (isMobile.phone ? window.innerHeight : 625) * ratio, 
+                    canvas.style.width = window.innerWidth + "px", canvas.style.height = isMobile.phone ? window.innerHeight : "625px", 
+                    c.setTransform(ratio, 0, 0, ratio, 0, 0);
+                });
+                for (var circleArray = [], i = 0; i < 300; i++) {
+                    var radius = 4 * Math.random() + 1, x = (Math.random() * (canvas.width - 2 * radius) + radius) / ratio, dx = Math.random() - .5, y = (Math.random() * (canvas.height - 2 * radius) + radius) / ratio, dy = Math.random() - .5;
+                    circleArray.push(new Circle(x, y, dx, dy, radius));
+                }
+                [ "01", "apple", "cn", "ebc", "eco", "et", "fb", "free", "gg", "gon", "ig", "line", "mirror", "now", "one", "ptt", "setn", "son", "tvbs", "udn", "us", "yahoo", "yt" ].forEach(function(obj) {
+                    var logoImg = new Image();
+                    logoImg.src = "img/media-" + obj + ".png";
+                    var radius = isMobile.phone ? Math.random() + 60 : Math.random() + 60, x = (Math.random() * (canvas.width - 2 * radius) + radius) / ratio, dx = Math.random() - .5, y = (Math.random() * (canvas.height - 2 * radius) + radius) / ratio, dy = Math.random() - .5;
+                    circleArray.push(new Circle(x, y, dx, dy, radius, !0, logoImg));
+                }), function animate() {
+                    requestAnimationFrame(animate), c.clearRect(0, 0, canvas.width, canvas.height);
+                    for (var i = 0; i < circleArray.length; i++) circleArray[i].update();
+                }();
             });
-            for (var circleArray = [], i = 0; i < 300; i++) {
-                var radius = 4 * Math.random() + 1, x = Math.random() * (canvas.width - 2 * radius) + radius, dx = Math.random() - .5, y = Math.random() * (canvas.height - 2 * radius) + radius, dy = Math.random() - .5;
-                circleArray.push(new Circle(x, y, dx, dy, radius));
-            }
-            [ "01", "apple", "cn", "ebc", "eco", "et", "fb", "free", "gg", "gon", "ig", "line", "mirror", "now", "one", "ptt", "setn", "son", "tvbs", "udn", "us", "yahoo", "yt" ].forEach(function(obj) {
-                var logoImg = new Image();
-                logoImg.src = "img/media-" + obj + ".png";
-                var radius = isMobile.phone ? Math.random() + 60 : Math.random() + 60, x = Math.random() * (canvas.width - 2 * radius) + radius, dx = Math.random() - .5, y = Math.random() * (canvas.height - 2 * radius) + radius, dy = Math.random() - .5;
-                circleArray.push(new Circle(x, y, dx, dy, radius, !0, logoImg));
-            }), function animate() {
-                requestAnimationFrame(animate), c.clearRect(0, 0, canvas.width, canvas.height);
-                for (var i = 0; i < circleArray.length; i++) circleArray[i].update();
-            }();
         },
         workEffect: function() {
             $(".work-video .desBox a.btn").fancybox(), $(".work-video .main .desBox").slick({
